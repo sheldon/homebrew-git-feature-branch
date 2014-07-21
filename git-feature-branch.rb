48bf7b0c7b6b1b45f3a1c0f5e1fad116b78cea11
Sheldon Els <sheldon.els@gmail.com>
2014-07-21 18:11:30 +0100

    added head version

--- git-feature-branch.rb
+++ git-feature-branch.rb
@@ -4,6 +4,7 @@ class GitFeatureBranch < Formula
   homepage "https://github.com/sheldon/git-feature-branch"
   url "https://github.com/sheldon/git-feature-branch/archive/v0.2.1.tar.gz"
   sha1 "ba5651d8a14bb691a4b8156b84430fa9580c75b0"
+  head "https://github.com/sheldon/git-feature-branch.git"
 
   def install
     bin.install "git-feature-branch"
