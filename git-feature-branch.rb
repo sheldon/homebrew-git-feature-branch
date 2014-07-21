44cc4a61020dc8b1f8a5894cd358e2aee4ed3820
Sheldon Els <sheldon.els@gmail.com>
2014-07-21 20:39:13 +0100

    v0.3

--- git-feature-branch.rb
+++ git-feature-branch.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GitFeatureBranch < Formula
   homepage "https://github.com/sheldon/git-feature-branch"
-  url "https://github.com/sheldon/git-feature-branch/archive/v0.2.1.tar.gz"
-  sha1 "ba5651d8a14bb691a4b8156b84430fa9580c75b0"
+  url "https://github.com/sheldon/git-feature-branch/archive/v0.3.tar.gz"
+  sha1 "b9e163ac848fb660680616064a8cb717cef110df"
   head "https://github.com/sheldon/git-feature-branch.git"
 
   def install
